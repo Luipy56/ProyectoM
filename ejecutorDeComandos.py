acab44e1e6a8d3856aba7c74254fa502cf34f084
Luipy <yoelberjaga@gmail.com>
2024-02-11 23:14:31 +0100

    COmando

--- ejecutorDeComandos.py
+++ ejecutorDeComandos.py
@@ -19,10 +19,17 @@ def ejecutarComandoDesdeArchivo(archivo):
         lineas = f.readlines()
         for linea in lineas:
             #Construir el comando seguido de la ruta
-            comando = f'dir "{linea}"'
+            comando = f'youtube-dl --geo-bypass -f bestvideo-bestaudio -o "RUTAAAAAA"'
 
             #Ejecutar el comando en la terminal
             subprocess.run(comando, shell=True)
 
+            # Eliminar la línea del archivo
+            with open(archivo, 'r') as f:
+                lines = f.readlines()
+            with open(archivo, 'w') as f:
+                for line in lines:
+                    if line.strip() != linea.strip():
+                        f.write(line)
 rutaArchivo = 'test.txt'
 ejecutarComandoDesdeArchivo(rutaArchivo)
